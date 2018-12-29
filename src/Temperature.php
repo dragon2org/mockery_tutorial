e03c19eae937757d90a9a711f41f7422c0bf5461
chengyuanlong <chengyuanlong@rsung.com>
2018-12-29 19:11:44 +0800

    init

---
+++ src/Temperature.php
@@ -0,0 +1,21 @@
+<?php
+namespace App;
+
+class Temperature
+{
+    private $service;
+
+    public function __construct($service)
+    {
+        $this->service = $service;
+    }
+
+    public function average()
+    {
+        $total = 0;
+        for ($i=0; $i<3; $i++) {
+            $total += $this->service->readTemp();
+        }
+        return $total/3;
+    }
+}
---
+++ tests/TemperatureTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Tests;
+
+use \Mockery;
+use PHPUnit\Framework\TestCase;
+use App\Temperature;
+
+class TemperatureTest extends TestCase
+{
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    public function testGetsAverageTemperatureFromThreeServiceReadings()
+    {
+        $service = Mockery::mock('service');
+        $service->shouldReceive('readTemp')
+            ->times(3)
+            ->andReturn(10, 12, 14);
+
+        $temperature = new Temperature($service);
+
+        $this->assertEquals(12, $temperature->average());
+    }
+}
